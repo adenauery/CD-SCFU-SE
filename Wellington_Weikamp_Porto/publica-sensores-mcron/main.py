9fe3de577eb239c61066e973dfe4b6947fb63047
wellkamp <wellkamp@gmail.com>
2022-11-18 18:54:10 -0300

    feat: add synchronize ntp job

--- Wellington_Weikamp_Porto/publica-sensores-mcron/main.py
+++ Wellington_Weikamp_Porto/publica-sensores-mcron/main.py
@@ -1,4 +1,5 @@
 from jobs.publish_dht11 import publish_dht11
+from jobs.synchronize_ntp import synchronize_ntp
 from utils.ntp import synchronize_time
 from utils.wifi_connection import connect
 import mcron
@@ -7,9 +8,10 @@ import mcron.decorators
 
 def main():
     connect()
-    mcron.insert(mcron.PERIOD_DAY, range(0, mcron.PERIOD_DAY, mcron.PERIOD_DAY // 2), 'day_x2', synchronize_time)
+    synchronize_time()
     mcron.init_timer()
     mcron.insert(mcron.PERIOD_HOUR, range(0, mcron.PERIOD_HOUR, mcron.PERIOD_HOUR // 6), '10_min', publish_dht11)
+    mcron.insert(mcron.PERIOD_DAY, range(0, mcron.PERIOD_DAY, mcron.PERIOD_DAY // 2), 'day_x2', synchronize_ntp)
     
   
 if __name__ == '__main__':
