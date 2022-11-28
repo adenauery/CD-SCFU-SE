4cc8baa8a47c67ed9f86e0cb410bc1e2cd43d396
Adenauer Yamin <adenauer@gmail.com>
2022-11-28 11:02:16 -0300

    Update le-imprime-bd-remoto.py

--- le-imprime-bd-remoto.py
+++ le-imprime-bd-remoto.py
@@ -1,7 +1,14 @@
 import mysql.connector
 
-minha_conexao_bd = mysql.connector.connect(host="scfu.exehda.org", user="scfur", passwd="ucpelpelotas", database="sensoriamento")
+minha_conexao_bd = mysql.connector.connect(host="scfu.exehda.org", user="scfur", passwd="XXXXXXXXXX", database="sensoriamento")
 meu_cursor = minha_conexao_bd.cursor()
+
+%    A password é a mesma utiliza no acesso abaixo:
+%    http://scfu.exehda.org/phpmyadmin/
+%    Utilizador: scfu
+%    Palavra-passe: XXXXXXXXXXXX
+%    Banco de Dados: sensoriamento
+
 
 sql = ("SELECT * FROM publicacoes WHERE origem='Profa Fernanda' ORDER BY publicacoes.datahora DESC LIMIT 10")
 meu_cursor.execute(sql)
